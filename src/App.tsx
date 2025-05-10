17bc889448ccef57da9273e7011306afdeae592d
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-05-11 00:20:00 +0100

    feature: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./screens/Auth/Login";
+import Signup from "./screens/Auth/Signup";
+import LandingPage from "./screens/LandingPage";
+import Dashboard from "./screens/Dashboard/Dashboard";
+import InterviewPrep from "./screens/InterviewPrep/InterviewPrep";
+import NotFound from "./screens/NotFound";
 
 const App = () => {
+  return (
+    <>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
 
-    return (
-        <>
-        <div className="text-5xl text-orange-700">App</div>
-        </>
-    );
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default App;
