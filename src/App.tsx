4f6f26902c7361508508b931b1f5697a97687e7f
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-05-10 23:06:53 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+    return (
+        <>
+        <div className="text-5xl text-orange-700">App</div>
+        </>
+    );
+};
+
+export default App;
